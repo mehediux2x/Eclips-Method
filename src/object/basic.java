2302bb29a66dc058760a84feb223a54434ad363d
mehediux2x <mehediux2x@yahoo.com>
2019-08-07 02:27:01 -0400

    first commit

---
+++ src/object/basic.java
@@ -0,0 +1,22 @@
+package object;
+
+public class basic {
+	public static void main(String[] args) {
+
+        String name = "mehedi";
+        String email = "mehediux2x@yahoo.com";
+        String Student_id = "3963NY";
+
+        //System.out.println(name001);
+
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(Student_id);
+
+
+
+
+
+    }
+
+}
